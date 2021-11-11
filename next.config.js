2d5987574ecdd89946f591169f83baf77c664330
Ningensei848 <k.kubokawa@klis.tsukuba.ac.jp>
2021-11-11 13:05:54 +0900

    Fix: Build error > Error: Specified basePath has to start with a /, found "jtd-validator"

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
+
 module.exports = {
   // cf. https://nextjs.org/docs/upgrading#swc-replacing-terser-for-minification
   swcMinify: true,
   // cf. https://nextjs.org/docs/api-reference/next.config.js/trailing-slash
   trailingSlash: true,
   // cf. https://nextjs.org/docs/api-reference/next.config.js/cdn-support-with-asset-prefix
-  assetPrefix: process.env.BASE_PATH || '',
+  assetPrefix: process.env.CDN_URL || `/${process.env.BASE_PATH}` || '',
   // cf. https://nextjs.org/docs/api-reference/next.config.js/basepath
-  basePath: process.env.BASE_PATH || '',
+  basePath: `/${process.env.BASE_PATH}` || '',
 }
