e21337b1c810257fb4dd0bc0260c8855d557c5c0
Ningensei848 <k.kubokawa@klis.tsukuba.ac.jp>
2021-11-11 16:35:39 +0900

    Fix: filepath resolution under `/public`

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,23 @@ import theme from 'src/theme'
 import createEmotionCache from 'src/createEmotionCache'
 import { GTM_ID } from 'src/google'
 import { GoogleTagManager, GoogleAdsense } from 'src/components/Google'
+import { url } from 'src/config'
+
+const Favicons = () => {
+  return (
+    <>
+      <link rel='apple-touch-icon' sizes='180x180' href={url('/favicons/apple-touch-icon.png')} />
+      <link rel='icon' type='image/png' sizes='32x32' href={url('/favicons/favicon-32x32.png')} />
+      <link rel='icon' type='image/png' sizes='16x16' href={url('/favicons/favicon-16x16.png')} />
+      <link rel='manifest' href={url('/favicons/site.webmanifest')} />
+      <link rel='mask-icon' href={url('/favicons/safari-pinned-tab.svg')} color='#059669' />
+      <link rel='shortcut icon' href={url('/favicon.ico')} />
+      <meta name='apple-mobile-web-app-title' content='JTD Validator' />
+      <meta name='application-name' content='JTD Validator' />
+      <meta name='msapplication-TileColor' content='#059669' />
+    </>
+  )
+}
 
 export default class MyDocument extends Document {
   render() {
@@ -14,19 +31,11 @@ export default class MyDocument extends Document {
         <Head>
           {/* PWA primary color */}
           <meta name='theme-color' content={theme.palette.primary.main} />
-          {/* <link
+          <link
             rel='stylesheet'
             href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
-          /> */}
-          <link rel='apple-touch-icon' sizes='180x180' href='/favicons/apple-touch-icon.png' />
-          <link rel='icon' type='image/png' sizes='32x32' href='/favicons/favicon-32x32.png' />
-          <link rel='icon' type='image/png' sizes='16x16' href='/favicons/favicon-16x16.png' />
-          <link rel='manifest' href='/favicons/site.webmanifest' />
-          <link rel='mask-icon' href='/favicons/safari-pinned-tab.svg' color='#059669' />
-          <link rel='shortcut icon' href='/favicon.ico' />
-          <meta name='apple-mobile-web-app-title' content='JTD Validator' />
-          <meta name='application-name' content='JTD Validator' />
-          <meta name='msapplication-TileColor' content='#059669' />
+          />
+          <Favicons />
           <GoogleTagManager />
           <GoogleAdsense />
         </Head>
---
+++ src/config.ts
@@ -0,0 +1,25 @@
+// 【魚拓】Next.js の public 以下のファイルのパスを正しく扱う｜まくろぐ
+// cf. https://megalodon.jp/2021-1111-1614-37/https://maku.blog:443/p/xjjbwes/
+
+import getConfig from 'next/config'
+
+/**
+ * public ディレクトリ以下に配置したファイルを参照するための URL を取得します。
+ *
+ * 例: url('/img/sample.png') => /reponame/img/sample.png
+ *
+ * next.config.js の publicRuntimeConfig.urlPrefix プロパティで指定した文字列が、
+ * filename 引数で指定された文字列のプレフィックスとして付加されます。
+ * 例えば Web サイトを GitHub Pages のプロジェクトページなどで公開する場合、
+ *
+ *   https://username.github.io/reponame/
+ *
+ * のように、URL のドメイン直下がアプリのルートにならないため、
+ * この URL 補正のために必要です。
+ *
+ * @see https://maku.blog/p/xjjbwes
+ */
+export function url(filename: string): string {
+    const { assetPrefix } = getConfig()
+    return assetPrefix + filename
+}
