2461b5d32edf7f23e3097bb8f6efde2c3788cdef
Ningensei848 <k.kubokawa@klis.tsukuba.ac.jp>
2021-11-11 16:01:07 +0900

    Update: modify favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,15 +14,19 @@ export default class MyDocument extends Document {
         <Head>
           {/* PWA primary color */}
           <meta name='theme-color' content={theme.palette.primary.main} />
-          <link
+          {/* <link
             rel='stylesheet'
             href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
-          />
+          /> */}
           <link rel='apple-touch-icon' sizes='180x180' href='/favicons/apple-touch-icon.png' />
           <link rel='icon' type='image/png' sizes='32x32' href='/favicons/favicon-32x32.png' />
           <link rel='icon' type='image/png' sizes='16x16' href='/favicons/favicon-16x16.png' />
           <link rel='manifest' href='/favicons/site.webmanifest' />
-          <link rel='mask-icon' href='/favicons/safari-pinned-tab.svg' color='#5bbad5' />
+          <link rel='mask-icon' href='/favicons/safari-pinned-tab.svg' color='#059669' />
+          <link rel='shortcut icon' href='/favicon.ico' />
+          <meta name='apple-mobile-web-app-title' content='JTD Validator' />
+          <meta name='application-name' content='JTD Validator' />
+          <meta name='msapplication-TileColor' content='#059669' />
           <GoogleTagManager />
           <GoogleAdsense />
         </Head>
