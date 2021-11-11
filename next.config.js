f209c30863cec4201259f10ad3cf46bd34c43076
Ningensei848 <k.kubokawa@klis.tsukuba.ac.jp>
2021-11-11 12:56:21 +0900

    Add: configuration for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  swcMinify: true
+  // cf. https://nextjs.org/docs/upgrading#swc-replacing-terser-for-minification
+  swcMinify: true,
+  // cf. https://nextjs.org/docs/api-reference/next.config.js/trailing-slash
+  trailingSlash: true,
+  // cf. https://nextjs.org/docs/api-reference/next.config.js/cdn-support-with-asset-prefix
+  assetPrefix: process.env.BASE_PATH || '',
+  // cf. https://nextjs.org/docs/api-reference/next.config.js/basepath
+  basePath: process.env.BASE_PATH || '',
 }
