1dcfec33e49e4d162c48472dd944b7257de959fe
Ningensei848 <k.kubokawa@klis.tsukuba.ac.jp>
2021-11-11 14:40:17 +0900

    Fix: path config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 
+const assetPrefix = process.env.BASE_PATH ? `/${process.env.BASE_PATH}` : ''
+
 module.exports = {
   // cf. https://nextjs.org/docs/upgrading#swc-replacing-terser-for-minification
   swcMinify: true,
   // cf. https://nextjs.org/docs/api-reference/next.config.js/trailing-slash
-  trailingSlash: true,
+  trailingSlash: process.env.BASE_PATH ? true : false,
   // cf. https://nextjs.org/docs/api-reference/next.config.js/cdn-support-with-asset-prefix
-  assetPrefix: process.env.CDN_URL || `/${process.env.BASE_PATH}` || '',
+  assetPrefix: process.env.CDN_URL || assetPrefix,
   // cf. https://nextjs.org/docs/api-reference/next.config.js/basepath
-  basePath: `/${process.env.BASE_PATH}` || '',
+  basePath: process.env.BASE_PATH ? `/${process.env.BASE_PATH}` : ''
 }
